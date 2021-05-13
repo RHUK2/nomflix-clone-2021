084dd98976a50bde129c584ddafcae20faeb2a2c
RHU5 <gusdnr814@naver.com>
2021-05-13 15:46:39 +0900

    TV Container request data

--- src/Routes/TV/TVContainer.js
+++ src/Routes/TV/TVContainer.js
@@ -1,3 +1,4 @@
+import { tvApi } from 'api';
 import React from 'react';
 import TVPresenter from './TVPresenter';
 
@@ -9,6 +10,39 @@ export default class extends React.Component {
     error: null,
     loading: true,
   };
+
+  async componentDidMount() {
+    try {
+      // console.log('first');
+      // tvApi.airingToday().then(({ data: { results } }) => {
+      //   console.log(results);
+      // });
+      // console.log('second');
+      const {
+        data: { results: airingToday },
+      } = await tvApi.airingToday();
+      const {
+        data: { results: topRated },
+      } = await tvApi.topRated();
+      const {
+        data: { results: popular },
+      } = await tvApi.popular();
+      this.setState({
+        airingToday,
+        topRated,
+        popular,
+      });
+    } catch {
+      this.setState({
+        error: "Can't find TV information",
+      });
+    } finally {
+      this.setState({
+        loading: false,
+      });
+    }
+  }
+
   render() {
     const { airingToday, topRated, popular, error, loading } = this.state;
     return (
